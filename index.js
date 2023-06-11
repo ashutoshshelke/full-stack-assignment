203adb2e5668264737a3bd13a928a653e63f5897
Ashutosh <ashutoshshelke07@gmail.com>
2023-06-11 08:36:27 +0530

    added signup code

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express')
-// const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken')
 
 const app = express()
 app.use(express.json())
 const port = 3001
 
-const USERS = [];
+const secretKey = 'secretkey'
+
+const USERS = [
+
+];
 
 const QUESTIONS = [{
   title: "Two states",
@@ -25,15 +29,24 @@ app.post('/signup', function (req, res) {
   // Add logic to decode body
   // body should have email and password
   const { username, password } = req.body;
-  console.log(username, password)
-
-
+  if (USERS.find(user => username === user.username))
+    return res.status(409).json({ mesage: `${username} username already exists. Try with new username` })
 
   //Store email and password (as is for now) in the USERS array above (only if the user with the given email doesnt exist)
 
+  // create new user
+  const newUser = {
+    id: USERS.length + 1,
+    username,
+    password
+  }
+
+  USERS.push(newUser)
+
+  const token = jwt.sign({ id: newUser.id, username: newUser.username }, secretKey)
 
   // return back 200 status code to the client
-  res.send('Hello World!')
+  res.status(200).json({ token, message: 'signup successfull. Please login', users: USERS })
 })
 
 app.post('/login', function (req, res) {
